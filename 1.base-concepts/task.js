1b432d24d30775c1c920d76610971f6a6593b900
meshi <mehhi@yandex.ru>
2023-01-28 07:15:54 +0300

    1.1.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if(discriminant === 0) {
+    const root = -b / (2 * a);
+    arr = [root];
+  }
+
+  if(discriminant > 0) {
+    const root1 = (-b + Math.sqrt(discriminant) )/(2*a);
+    const root2 = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr = [root1, root2];
+  }
+
   return arr;
 }
 
