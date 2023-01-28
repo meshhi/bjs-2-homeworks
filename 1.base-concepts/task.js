3f6bc7ce302045be25d3eb6b04f4078ba1cd6bed
meshi <mehhi@yandex.ru>
2023-01-28 07:29:34 +0300

    1 tests success

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,5 +19,14 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (Number.isNaN(Number(percent) + Number(contribution) + Number(amount) + Number(countMonths))) {
+    return false;
+  }
+
+  const percentage = percent / 100 / 12;
+  const creditAmount = amount - contribution;
+  const monthPayment = creditAmount * (percentage + (percentage / (((1 + percentage) ** countMonths) - 1)));
+  const resultPayment = monthPayment * countMonths;
+
+  return Number(resultPayment.toFixed(2));
 }
