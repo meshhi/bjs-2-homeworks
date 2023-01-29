300a1587571c43479ad993b4939f16a9de2b4642
meshi <mehhi@yandex.ru>
2023-01-30 00:27:55 +0300

    6 tests success

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿const parseCount = (value) => {
+  if (Number.isNaN(Number.parseFloat(value))) {
+    throw new Error("Невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+
+const validateCount = (value) => {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if ((a + b < c) || (b + c < a) || (c + a < b)) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  };
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    return Number(Math.sqrt(this.perimeter/2 * (this.perimeter/2 - this.a) * (this.perimeter/2 - this.b) * (this.perimeter/2 - this.c)).toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try{
+    return new Triangle(a, b, c);
+  } catch(err){
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
+
+new Triangle(1,3,3)
